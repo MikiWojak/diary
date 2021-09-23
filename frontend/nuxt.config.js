b889fe2d5d9b6757b4ea4de9a876656c9678e862
MikiWojak <mikizarowa8@gmail.com>
2021-09-23 18:04:40 +0200

    Add empty line and delete comments

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         title: 'frontend',
         htmlAttrs: {
@@ -17,30 +16,17 @@ export default {
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     },
 
-    // Global CSS: https://go.nuxtjs.dev/config-css
     css: [],
 
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
     components: false,
 
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-        // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module'
-    ],
+    buildModules: ['@nuxtjs/eslint-module'],
 
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios'
-    ],
+    modules: ['@nuxtjs/axios'],
 
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {},
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {}
 };
